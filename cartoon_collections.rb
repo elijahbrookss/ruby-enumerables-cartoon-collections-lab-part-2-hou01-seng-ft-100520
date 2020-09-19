0311b268c5a87bf837cc64b3a7b7dbdaf637185b
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-19 14:42:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.all? {|element| element.length > 4}
+  planeteer_calls.any? {|element| element.length > 4}
   
 end
 
